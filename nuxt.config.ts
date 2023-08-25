2b0b73a7e58cb2b867826db7ff6dfe143849fb4e
fayazara <fayazara@gmail.com>
2023-08-25 09:30:22 +0530

    Removed PWA because the feeds done showeven when you are logged in

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,6 @@
-import icons from "./constants/icons.json";
-import splashScreens from "./constants/apple-pwa-links.json";
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: [
-    "@nuxthq/ui",
-    "nuxt-icon",
-    "@nuxtjs/supabase",
-    "@vueuse/nuxt",
-    "@vite-pwa/nuxt",
-  ],
+  modules: ["@nuxthq/ui", "nuxt-icon", "@nuxtjs/supabase", "@vueuse/nuxt"],
   ui: {
     icons: ["heroicons", "logos", "lucide"],
   },
@@ -21,32 +13,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       baseUrl: process.env.BASE_URL || "http://localhost:3000",
-    },
-  },
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1, user-scalable=no",
-      link: [...splashScreens],
-    },
-  },
-  pwa: {
-    manifest: {
-      name: "Feedful",
-      short_name: "Feedful",
-      description: "News for the curious",
-      icons,
-      display: "standalone",
-      orientation: "portrait",
-      theme_color: "#ffffff",
-    },
-    workbox: {
-      navigateFallback: "/",
-    },
-    devOptions: {
-      enabled: true,
-
-      type: "module",
     },
   },
 });
