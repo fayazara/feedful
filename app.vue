a9f8bbca581a60ba6d62c4459c3a8be93f1b8e5b
fayazara <fayazara@gmail.com>
2023-08-27 11:46:03 +0530

    SEO

--- app.vue
+++ app.vue
@@ -3,3 +3,16 @@
   <NuxtPage />
   <UNotifications />
 </template>
+
+<script setup>
+useSeoMeta({
+  title: "Feedful - a modern news reader",
+  ogTitle: "Feedful - a modern news reader",
+  description:
+    "A tweet deck styled modern news reader that helps you stay up to date with your favorite websites.",
+  ogDescription:
+    "A tweet deck styled modern news reader that helps you stay up to date with your favorite websites.",
+  ogImage: "https://feedful.app/banner.png",
+  twitterCard: "summary_large_image",
+});
+</script>
