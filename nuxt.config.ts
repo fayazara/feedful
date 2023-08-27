6bda7d884ec8302273d73f8aef085d907b0f809b
fayazara <fayazara@gmail.com>
2023-08-27 11:24:34 +0530

    Vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,18 @@ export default defineNuxtConfig({
   supabase: {
     redirect: false,
   },
+  app: {
+    head: {
+      title: "Feedful - a modern news reader",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Feedful is a tweet deck styled modern news reader that helps you stay up to date with your favorite websites.",
+        },
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       baseUrl: process.env.BASE_URL || "http://localhost:3000",
