1d70434753c6c7e5abc67ff7815cc7fc5e911c59
fayazara <fayazara@gmail.com>
2023-08-31 10:53:45 +0530

    Added Umami Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,15 @@ export default defineNuxtConfig({
       baseUrl: process.env.BASE_URL || "http://localhost:3000",
     },
   },
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://analytics.umami.is/script.js",
+          "data-website-id": "0e36663c-ef9c-42c5-a147-7c0d993ab160",
+          async: true,
+        },
+      ],
+    },
+  },
 });
